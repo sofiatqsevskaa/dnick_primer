ffc3d5807a0d919cd334878345b130427bb6fc30
sofiatqsevskaa <tasevska.sofija@yahoo.com>
2025-05-24 22:46:11 +0200

    added models,admin

--- primer/admin.py
+++ primer/admin.py
@@ -1,3 +1,57 @@
+from multiprocessing.resource_tracker import register
+
 from django.contrib import admin
+from django.db.models import Count
+from .models import Cake, Baker
 
 # Register your models here.
+
+class BakerAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser
+
+    def get_queryset(self, request):
+        qs = super(BakerAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs.annotate(cakes_count=Count('cakes')).filter(cakes_count__lt=5)
+        return qs
+
+class CakeAdmin(admin.ModelAdmin):
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj = None):
+        return obj and obj.baker.user == request.user
+
+    def save_model(self, request, obj, form, change):
+        baker = Baker.objects.filter(user=request.user).first()
+        baker_cakes = Cake.objects.filter(baker=baker).all()
+
+        if not change and baker_cakes.count() == 10:
+            return
+
+        sum_ = 0
+        for cake in baker_cakes:
+            sum_ += cake.price
+
+        old_cake_obj = baker_cakes.filter(id = obj.id).first()
+
+        if not change and sum_+obj.price >10000:
+            return
+
+        if change and sum_+obj.price-old_cake_obj.price > 10000:
+            return
+
+        if Cake.objects.filter(name=obj.name).exists():
+            return
+
+        super(CakeAdmin, self).save_model(request, obj, form, change)
+
+admin.site.register(Cake, CakeAdmin)
+admin.site.register(Baker, BakerAdmin)
--- primer/models.py
+++ primer/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Baker(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='bakers/', null=True, blank=True)
+
+class Cake(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    weight = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="cakes/", null=True, blank=True)
+    baker = models.ForeignKey(Baker, on_delete=models.CASCADE, related_name="cakes", blank=True, null=True)
+
